347d49038a7427f77b1081661afe04aa555c75b2
nimeshmohan <nimeshvellinezhi@gmail.com>
2024-02-09 17:29:34 +0530

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 1445;
+let accountEmail = 'nimeshvellinezhi@gmail.com';
+var accountPassword = '12345'
+
+accountCity = 'kerala';
+let accountState;
+
+// accountId = 18564 // not allowed 
+accountEmail = 'nimesh.02clssh@gmail.com'
+accountPassword = '23546'
+
+accountCity = 'chennai';
+
+console.log(accountEmail);
+
+console.table([accountEmail,accountPassword,accountCity,accountState])
+
+/* prefer not to use var 
+ becoause of issue in block scope and functional scope */
